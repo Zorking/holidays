48e47d01a40f7e1b9e92016634545e67dfa0b22a
Vadim Fabrichnov <v.fabrichnov@deliveryweb.ru>
2019-02-24 19:32:31 +0300

    New style

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
     <div id="app">
-        <app-nav></app-nav>
-        <div class="app-content container" >
-            <router-view></router-view>
+        <div class="app-content container">
+            <router-view v-if="this.isAppWorking"></router-view>
+            <div class="pokemon-theme" v-else>Приложение не работает с 11 до 19</div>
         </div>
     </div>
 </template>
 
 <script>
-    import AppNav from './AppNav.vue'
-
     export default {
         name: 'app',
-        components: {AppNav},
         data() {
             return {
                 bread: this.$router.name,
+                isAppWorking: 11 <= new Date().getHours() && new Date().getHours() >= 19,
             }
-        }
+        },
     }
 </script>
 
@@ -25,8 +23,9 @@
 
     @import '../node_modules/bootstrap/scss/bootstrap';
 
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
     #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-family: "Roboto Condensed";
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
@@ -43,4 +42,19 @@
         position: relative;
         margin: 0;
     }
+
+    .pokemon-theme {
+        border-radius: 10px;
+        background: #F8F8D8;
+        border: 6px solid #506860;
+        margin-top: 100px;
+        padding: 20px;
+    }
+
+    .btn, .btn:hover {
+        border-color: #506860;
+        border-width: 1px;
+    }
+
+
 </style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,44 +1,76 @@
 <template>
     <div>
-        <div class="form-group">
-            <label for="fio">ФИО</label>
-            <input class="form-control" id="fio">
+        <div class="pokemon-theme" id="main-form">
+            <div class="form-group">
+                <label for="fio">ФИО</label>
+                <input class="form-control" id="fio">
+            </div>
+            <label for="exampleInputEmail1">Электронная почта</label>
+            <input type="email" class="form-control" id="exampleInputEmail1">
+            <p class="gender">Пол</p>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="male">
+                <label class="form-check-label" for="exampleRadios1">
+                    Мужской
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="female">
+                <label class="form-check-label" for="exampleRadios2">
+                    Женский
+                </label>
+            </div>
+            <button type="submit" class="btn btn-warning" @click="sendForm">Отправить</button>
         </div>
-        <label for="exampleInputEmail1">Электронная почта</label>
-        <input type="email" class="form-control" id="exampleInputEmail1">
-        <p>Пол</p>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-            <label class="form-check-label" for="inlineRadio1">Мужской</label>
+        <div class="pokemon-theme" id="wait" style="display: none">
+            Подождите...
         </div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-            <label class="form-check-label" for="inlineRadio2">Женский</label>
-        </div>
-        <br>
-        <button type="submit" class="btn btn-primary" @click="sendForm">Отправить</button>
     </div>
 </template>
 
 <script>
+
     export default {
         name: "Form",
         methods: {
             sendForm: function () {
                 let name = document.getElementById('fio').value;
                 let email = document.getElementById('exampleInputEmail1').value;
-                let isFemale = !document.getElementById('inlineRadio1').value;
+                let isFemale = document.getElementById('exampleRadios2').checked;
                 let q = {
                     "fio": name,
                     "email": email,
                     "is_female": isFemale
                 };
-                this.$http.post('http://127.0.0.1:8000/form/', q, {emulateJSON:true}).then(alert("Успех"));
+                let mainForm = document.getElementById('main-form');
+                let wait = document.getElementById('wait');
+                mainForm.style.display = 'none';
+                wait.style.display = '';
+                this.$http.post('http://127.0.0.1:8000/form/', q, {emulateJSON: true})
+                    .then(function () {
+                        this.$router.push('/');
+                    })
+                    .catch(function (error) {
+                        mainForm.style.display = '';
+                        wait.style.display = 'none';
+                        alert(JSON.stringify(error.data));
+                    });
             }
         }
     }
 </script>
 
 <style scoped>
+    .form-control {
+        border-color: #506860;
+    }
 
+    .gender {
+        top: 10px;
+        position: relative;
+    }
+
+    .btn {
+        margin-top: 15px;
+    }
 </style>
